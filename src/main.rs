65b1ebeb53bdee910c702aa24082c4050d7e336e
mr-crunch <mattbianco4@gmail.com>
2025-02-23 11:21:11 -0600

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+mod human_input {
+    use std::{
+        io::{self, Write},
+        str::FromStr,
+    };
+
+    pub fn read_string(prompt: &str) -> io::Result<Option<String>> {
+        print!("{}", prompt);
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        Ok(if input.trim() == "" {
+            None
+        } else {
+            Some(input.trim().to_string())
+        })
+    }
+
+    pub fn read_string_checked(prompt: &str) -> io::Result<String> {
+        loop {
+            match read_string(prompt)? {
+                Some(input) => return Ok(input),
+                None => println!("input cannot be empty"),
+            }
+        }
+    }
+
+    pub fn read_typed_checked<T: FromStr>(prompt: &str) -> io::Result<T> {
+        loop {
+            let untyped_input = read_string_checked(prompt)?;
+            match untyped_input.parse::<T>() {
+                Ok(input) => return Ok(input),
+                Err(_) => println!("{} is not valid", untyped_input),
+            }
+        }
+    }
+
+    pub fn read_menu(
+        prompt: &str,
+        options: &[impl AsRef<str>],
+        default: Option<usize>,
+    ) -> io::Result<usize> {
+        assert!(
+            if let Some(num) = default {
+                num < options.len()
+            } else {
+                true
+            },
+            "default index must be in options slice"
+        );
+        loop {
+            print!("{}", prompt);
+        }
+    }
+}
+
+fn main() {
+    println!("Hello world!");
+    println!("input: {}", input());
+}
+
+fn input() -> String {
+    match human_input::read_string_checked("enter name: ") {
+        Ok(string) => string,
+        Err(error) => {
+            eprintln!("error: {:?}", error);
+            String::from("error")
+        }
+    }
+}
